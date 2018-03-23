c3e9cfd92811c989e71343484186689f69a09452
zhang5690889 <xinwen.zhang.911@gmail.com>
2018-03-23 13:33:31 -0400

    Fixed mkdir dependency

--- pydfs/utils.py
+++ pydfs/utils.py
@@ -6,7 +6,7 @@ CONFIG_PATH = 'dfs.conf'
 LOG_DIR = '/tmp/minion/log'
 # create logging directory if not exist
 if not os.path.isdir(LOG_DIR):
-    os.system('mkdir -p {}'.format(LOG_DIR))
+    os.makedirs(LOG_DIR)
 
 def get_master_config():
     config = ConfigParser()
